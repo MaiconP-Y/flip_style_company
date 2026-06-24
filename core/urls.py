65df64e8d07e1dce43a75d733164d3447746646a
MaiconP-Y <maiconwantuil@gmail.com>
2026-06-24 15:29:07 -0300

    feat(pages): add about-us and privacy-policy institutional views

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from products.views import home
+from products.views import home, ProductsListView, QuemSomosView, politica_privacidade
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('quem_somos/', QuemSomosView, name='quem_somos'),
+    path('politica-de-privacidade/', politica_privacidade, name='privacidade'),
+    path('produtos/', ProductsListView.as_view(), name='products'),
+    path('produtos/categoria/<slug:category_slug>/', ProductsListView.as_view(), name='category'),
+    
+    # Quando clicar na Subcategoria (Ex: /produtos/sub/camisetas/)
+    path('produtos/sub/<slug:subcategory_slug>/', ProductsListView.as_view(), name='subcategory'),
 ]
 
 # Apenas adicione as rotas de mídia se estivermos em desenvolvimento
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic import ListView
+from .models import Product, Color, Size, Brand
 
 def home(request):
     # 1. Busca os produtos de destaque
@@ -8,14 +10,14 @@ def home(request):
         variants__stock__gt=0
     ).distinct().prefetch_related('images')[:10]
 
-    # 2. Definição da lista estática de categorias
+    # Em views.py, dentro da função home()
     categorias = [
-        {'name': 'Calças', 'image': 'img/categories/calcas.webp'},
-        {'name': 'Bonés', 'image': 'img/categories/bones.webp'},
-        {'name': 'Toucas', 'image': 'img/categories/toucas.webp'},
-        {'name': 'Bermudas', 'image': 'img/categories/bermudas.webp'},
-        {'name': 'Meias', 'image': 'img/categories/meias.webp'},
-        {'name': 'Blusas', 'image': 'img/categories/moletons.webp'},
+        {'name': 'Calças', 'image': 'img/categories/calcas.webp', 'slug': 'calcas'},
+        {'name': 'Bonés', 'image': 'img/categories/bones.webp', 'slug': 'bones'},
+        {'name': 'Toucas', 'image': 'img/categories/toucas.webp', 'slug': 'toucas'},
+        {'name': 'Bermudas', 'image': 'img/categories/bermudas.webp', 'slug': 'bermudas'},
+        {'name': 'Meias', 'image': 'img/categories/meias.webp', 'slug': 'meias'},
+        {'name': 'Blusas', 'image': 'img/categories/moletons.webp', 'slug': 'blusas'},
     ]
 
     marcas = [
@@ -34,3 +36,85 @@ def home(request):
         'categorias': categorias,
         'marcas': marcas # Enviando para o template
     })
+
+class ProductsListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'products.html'
+    
+    def get_queryset(self):
+        queryset = Product.objects.all().select_related('brand').prefetch_related('images', 'variants__size')
+        
+        # 2. Filtros de Hierarquia (URL)
+        category_slug = self.kwargs.get('category_slug')
+        subcategory_slug = self.kwargs.get('subcategory_slug')
+        
+        if subcategory_slug:
+            queryset = queryset.filter(subcategory__slug=subcategory_slug)
+        elif category_slug:
+            queryset = queryset.filter(subcategory__category__slug__iexact=category_slug)
+        
+        # 3. BUSCA
+        search_query = self.request.GET.get('search')
+        if search_query:
+            queryset = queryset.filter(name__icontains=search_query)
+
+        # 4. Filtros de QueryString
+        brand_slug = self.request.GET.get('marca')
+        cor_slug = self.request.GET.get('cor')
+        tamanho_slug = self.request.GET.get('tamanho')
+        
+        if brand_slug:
+            queryset = queryset.filter(brand__slug=brand_slug)
+        if cor_slug:
+            queryset = queryset.filter(color__slug=cor_slug)
+        if tamanho_slug:
+            queryset = queryset.filter(variants__size__slug=tamanho_slug)
+
+        # 5. Ordenação
+        ordenacao = self.request.GET.get('preco')
+        if ordenacao == 'asc':
+            queryset = queryset.order_by('price')
+        elif ordenacao == 'desc':
+            queryset = queryset.order_by('-price')
+        else:
+            queryset = queryset.order_by('-created_at')
+            
+        # .distinct() é importante se você tiver filtros com JOINs complexos
+        return queryset.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Contexto base
+        context['marcas'] = Brand.objects.all().order_by('name')
+        context['cores'] = Color.objects.all()
+        
+        # Slugs para o template
+        sub_slug = self.kwargs.get('subcategory_slug')
+        context['category_slug'] = self.kwargs.get('category_slug')
+        context['subcategory_slug'] = sub_slug
+        
+        # Lógica inteligente de filtro de tamanhos
+        todos_tamanhos = Size.objects.all().order_by('order')
+        
+        tamanhos_letras = ['camisetas', 'blusas'] 
+        tamanhos_numeros = ['footwears', 'calcas']
+
+        if sub_slug in tamanhos_letras:
+            # Filtra apenas letras (P, M, G...)
+            context['tamanhos'] = [s for s in todos_tamanhos if not s.name.isdigit()]
+        elif sub_slug in tamanhos_numeros:
+            # Filtra apenas números (38, 40, 42...)
+            context['tamanhos'] = [s for s in todos_tamanhos if s.name.isdigit()]
+        else:
+            # Se não estiver em nenhuma, mostra todos (ou o que fizer sentido para você)
+            context['tamanhos'] = todos_tamanhos
+        
+        return context
+    
+def QuemSomosView(request):
+    return render(request, 'quem_somos.html')
+
+def politica_privacidade(request):
+    return render(request, 'politica-de-privacidade.html')
