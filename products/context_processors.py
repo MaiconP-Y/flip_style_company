82cb685f6285c9e868bfbb1061e5264f6d6c37fb
MaiconP-Y <maiconwantuil@gmail.com>
2026-07-02 20:49:42 -0300

    fix(menu): filtrar modelos do menu para exibir apenas a categoria footwears

--- products/context_processors.py
+++ products/context_processors.py
@@ -1,10 +1,12 @@
 from django.db.models.functions import Lower
+from django.db.models import Q
 from .models import ProductModel
 
 def modelos_menu(request):
-    # Busca apenas os modelos que possuem produtos com alguma variante em estoque
     modelos_ativos = ProductModel.objects.filter(
         color_variants__variants__stock__gt=0
+    ).filter(
+        Q(subcategory__category__slug='footwears') | Q(subcategory__slug='footwears')
     ).distinct().order_by(Lower('name'))
     
     return {
